0e809e3cf0ebdca12e04583aaf1895ad3908f7d9
VrishtiSinghal <87196707+VrishtiSinghal@users.noreply.github.com>
2021-07-09 20:45:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function add(){
+var result= Number(document.getElementById("firstno").value)+
+Number(document.getElementById("secondno").value);
+window.alert("The sum is: "+ result);
+}
+
+function subtract(){
+var result= Number(document.getElementById("firstno").value)-
+Number(document.getElementById("secondno").value);
+window.alert("The Difference is: "+ result);
+}
+
+function multiply(){
+var result= Number(document.getElementById("firstno").value)*
+Number(document.getElementById("secondno").value);
+window.alert("The product is: "+ result);
+}
+
+function divide(){
+var result= Number(document.getElementById("firstno").value)/
+Number(document.getElementById("secondno").value);
+window.alert("The Quotient is: "+ result);
+}
